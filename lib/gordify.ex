3e66eb453cf16d8afecd1ed7b1a9de83186e76ae
Rafael Ramón Fraga <rafael@cabify.com>
2017-03-17 13:49:33 +0100

    first commit

---
+++ lib/gordify.ex
@@ -0,0 +1,18 @@
+defmodule Gordify do
+  @moduledoc """
+  Documentation for Gordify.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Gordify.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/gordify/application.ex
@@ -0,0 +1,24 @@
+defmodule Gordify.Application do
+  # See http://elixir-lang.org/docs/stable/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    slack_token = Application.get_env(:gordify, Gordify.Slack)[:token]
+
+    # Define workers and child supervisors to be supervised
+    children = [
+      # Starts a worker by calling: Gordify.Worker.start_link(arg1, arg2, arg3)
+      worker(Slack.Bot, [Gordify.Slack, [], slack_token]),
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Gordify.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
